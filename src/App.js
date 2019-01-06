1072dcd9bf7cf3b2cf1fc6cf0dd98dd132b2973d
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 11:29:40 +0100

    Embedd OpenWeatherMap api key as comment

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
-
 import './App.css';
+
+// const API_KEY = 730350ee1e4612d1b79e8805dea7d388 (OpenWeatherMap API)
 
 class App extends Component {
   render() {
