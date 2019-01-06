1f8f69e83e6f111ce426e670bb4d92c5553fc93c
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 11:44:27 +0100

    Create form element with req input fields within Form component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import './App.css';
 
 const API_KEY = '730350ee1e4612d1b79e8805dea7d388';
 
-// api url: http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=730350ee1e4612d1b79e8805dea7d388
-
 class App extends Component {
   getWeather = async () => {
     const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <div>
-        Form Component!
-      </div>
+      <form>
+        <input type="text" name="city" placeholder="City..."/>
+        <input type="text" name="country" placeholder="Country..."/>
+        <button>Get Weather</button>
+      </form>
     )
   }
 }
