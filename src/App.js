f8f2c934d736cdf8a400fbe4960bc1c095e01677
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 12:13:01 +0100

    Move the getWeather method to the Form component and return data to the parent App component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 import './App.css';
 
-const API_KEY = '730350ee1e4612d1b79e8805dea7d388';
-
 class App extends Component {
-  getWeather = async () => {
-    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
-    const apiData = await apiCall.json();
-    console.log(apiData);
+  displayData = data => {
+    console.log(data);
   }
 
   render() {
     return (
       <div>
         <Titles />
-        <Form getWeather={this.getWeather}/>
+        <Form returnApiResponse = {(data) => this.displayData(data)}/>
         <Weather />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+const API_KEY = '730350ee1e4612d1b79e8805dea7d388';
 
 class Form extends Component {
-  executeForm = (e) => {
+  getWeather = async (e) => {
     e.preventDefault();
-    this.props.getWeather();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+
+    if (apiCall) {
+      const apiData = await apiCall.json();
+      this.props.returnApiResponse(apiData); // return data to the app component
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.executeForm}>
+      <form onSubmit={this.getWeather}>
         <input type="text" name="city" placeholder="City..."/>
         <input type="text" name="country" placeholder="Country..."/>
         <button type="submit">Get Weather</button>
