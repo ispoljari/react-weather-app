f8b153b393cac034789385dd97731a8d569b2a04
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 11:33:44 +0100

    Embedd OpenWeatherMap api url as comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weather from './components/Weather';
 import './App.css';
 
 // const API_KEY = 730350ee1e4612d1b79e8805dea7d388 (OpenWeatherMap API)
+// api url: http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=730350ee1e4612d1b79e8805dea7d388
 
 class App extends Component {
   render() {
