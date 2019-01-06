4a7da87b980cf325f9900b2197ff0ffc228464b0
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 11:16:56 +0100

    Create Weather component, mount on App and render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
+import Weather from './components/Weather';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   }
