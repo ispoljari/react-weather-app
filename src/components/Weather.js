b31eb56cd71184d18d432d71195bfe3fd84e2fd6
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 16:43:11 +0100

    Hide Weather component elements when no data present

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,11 @@ class Weather extends Component {
   render() {
     return (
       <div>
-        <p>Location: { this.props.apiData.city }, { this.props.apiData.country } </p>
-        <p>Temperature: { this.props.apiData.temperature }</p>
-        <p>Humidity: { this.props.apiData.humidity }</p>
-        <p>Conditions: { this.props.apiData.description }</p>
+        { this.props.apiData.city && this.props.apiData.country && 
+        <p>Location: { this.props.apiData.city }, { this.props.apiData.country } </p> }
+        { this.props.apiData.temperature && <p>Temperature: { this.props.apiData.temperature }</p> }
+        { this.props.apiData.humidity && <p>Humidity: { this.props.apiData.humidity }</p> }
+        { this.props.apiData.description && <p>Conditions: { this.props.apiData.description }</p> }
       </div>
     );
   }
