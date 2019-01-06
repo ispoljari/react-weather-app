55590c366bc1f894babceeafe758e7b4d77bb046
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-06 11:08:56 +0100

    Create Titles component

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
- // some code
-}
+  render() {
+    return (
+      <div>Hello!</div>
+    );
+  }
+};
+
+export default App;
